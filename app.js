99bdd47d69232bbee5ea17b17bc7f3031fd82d7e
nahidappltd@gmail.com <nahidappltd@gmail.com>
2024-01-19 17:04:36 +0600

    new update

--- app.js
+++ app.js
@@ -1,47 +1,54 @@
 console.log('Hello World!');
+
 var date = document.getElementById("date");
-var title = document.getElementById("title");
+
+
 var note = document.getElementById("note");
-function dateset(){
- var today = new Date();
- 
- var day = today.getDate();
- var month = today.getMonth() + 1;
- var year = today.getFullYear();
- 
- var vdate = day + "/" + month + "/"+ year;
-  date.value= vdate;
+
+function dateset() {
+  var today = new Date();
+
+  var day = today.getDate();
+  var month = today.getMonth() + 1;
+  var year = today.getFullYear();
+
+  var vdate = day + "/" + month + "/" + year;
+  date.value = vdate;
 }
 
 function setstoreg() {
- dateset();
- var dateu = document.getElementById("date").value;
-localStorage.setItem('1date',dateu);
+  dateset();
+  var dateu = document.getElementById("date").value;
+  localStorage.setItem('1date', dateu);
 }
 
-function backdate(){
+function backdate() {
   var bdate = localStorage.getItem("1date");
-  
-    date.value= bdate;
+
+  date.value = bdate;
 
 }
 backdate();
 
-function savetitle(){
-var valeutitle = title.value;
-localStorage.setItem('title',valeutitle);
+function savetitle() {
+var titleData =  document.getElementById("title5").value;
+  localStorage.setItem('title', titleData);
 }
-function logger(){
+
+function logger() {
   var rust = localStorage.getItem('title');
-  title.value= rust;
+ document.getElementById("title5").value = rust;
+
 }
 logger();
-function onnote(){
-var valuenote = note.value;
-localStorage.setItem('note',valuenote);
+
+function onnote() {
+  var valuenote = note.value;
+  localStorage.setItem('note', valuenote);
 }
+
 function setnote(e) {
-var servernote =   localStorage.getItem('note');
+  var servernote = localStorage.getItem('note');
   note.value = servernote;
 }
 setnote();
